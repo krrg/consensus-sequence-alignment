a679ba4e209ab0b0c240c416062d3a16156d36e9
Ken <krrgit@gmail.com>
2014-10-30 17:14:08 -0600

    Added the netbeans project.

---
+++ Secali/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: krr428
+ *
+ * Created on October 30, 2014, 5:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
